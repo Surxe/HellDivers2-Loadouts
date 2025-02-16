d351ea08e6e5099884262b6db15978b323f51a1a
Surxe <119145352+Surxe@users.noreply.github.com>
2025-02-15 19:07:33 -0500

    Test param passing from ahk to py

--- src/selector/Build.py
+++ src/selector/Build.py
@@ -1,6 +1,13 @@
 class Build():
     def __init__(self, build_path):
         self.build_path = build_path
-        
+        self.keybinds = []
+
+    def print_keybinds(self):
+        for keybind in self.keybinds:
+            print(keybind)
 
     def read_build_file(self):
+        self.keybinds = ["keybind1", "keybind2", "keybind3", "keybind4"]
+
+    #def read_build_file(self):
