26d38a30185b7ccc6ab5da74a5979241343bd454
Surxe <119145352+Surxe@users.noreply.github.com>
2025-02-22 16:10:23 -0500

    Hide Create Loadout button while creating a loadout

--- src/builder/static/js/create_loadout.js
+++ src/builder/static/js/create_loadout.js
@@ -1,5 +1,9 @@
 async function create_loadout() {
     try {
+        // Hide the Create Loadout button
+        create_btn = document.getElementById("create_loadout_btn");
+        create_btn.style.display = "none";
+
         const response = await fetch("/create_loadout", {
             method: "POST",
             headers: { "Content-Type": "application/json" }
@@ -7,12 +11,18 @@ async function create_loadout() {
 
         const data = await response.json();
         if (response.ok) {
-            rename_loadout(data.loadout_name, true); // Rename the current loadout
+            await rename_loadout(data.loadout_name, true); // Rename the current loadout
         } else {
             alert("Error: " + data.error);
         }
+
+        // Show the Create Loadout button again
+        create_btn.style.display = "block";
     } catch (error) {
         console.error("Error:", error);
+    } finally {
+        // Ensure the Create Loadout button is shown again
+        create_btn.style.display = "block";
     }
 }
 
