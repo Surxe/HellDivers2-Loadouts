00973dfe0f03524ee20de13fcfe16964d2dec0e5
Surxe <119145352+Surxe@users.noreply.github.com>
2025-02-24 16:14:28 -0500

    save and load methods to classes; create and save in create_objects.py, load in asset_grabber.py

--- src/equipment/Equipment.py
+++ src/equipment/Equipment.py
@@ -6,8 +6,11 @@ sys.path.append(parent_dir)
 
 # Import external libraries
 import os
+import json
 
 class Equipment:
+    objs = {}
+
     def __init__(self, id, name, wiki_file_name, file_extension='png', parent_dirs=""):
         self.id = id
         self.name = name
@@ -18,3 +21,11 @@ class Equipment:
     def asset_exists(self):
         path = os.path.join("src/assets", self.parent_dirs, f"{self.id}.{self.file_extension}")
         return os.path.exists(path)
+    
+    @classmethod
+    def save(cls):
+        with open(cls.path, 'w', encoding='utf-8') as f:
+            data = dict()
+            for id, obj in cls.objs.items():
+                data[id] = obj.__dict__
+            f.write(json.dumps(data, indent=4))
--- src/equipment/Weapon.py
+++ src/equipment/Weapon.py
@@ -4,11 +4,16 @@ import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
+# Import external libraries
+import json
+
 # Import local libraries
 from equipment.Equipment import Equipment
 
 # Make weapon class that inherits from Equipment
 class Weapon(Equipment):
+    path = "src/equipment/data/weapons.json"
+    objs = {}
     valid_slots = ['Primary', 'Secondary', 'Throwable']
     valid_categories = {
         "Primary": ["Assault Rifle", "Marksman Rifle", "Sniper Rifle", "Submachine Gun", "Shotgun", "Explosive", "Energy-Based", "Special"],
@@ -37,5 +42,20 @@ class Weapon(Equipment):
         if category not in self.valid_categories[slot]:
             raise ValueError(f"For weapon \"{self.id}\", Invalid category \"{category}\" for slot {slot}, must be one of {self.valid_categories[slot]}")
         self.category = category
-
         
+        Weapon.objs[id] = self
+
+    @classmethod
+    def load(cls):
+        with open(cls.path, 'r') as f:
+            objs = json.load(f)
+            for id, obj in objs.items():
+                cls(
+                    id,
+                    obj['name'],
+                    obj['slot'],
+                    obj['category'],
+                    obj['wiki_file_name'],
+                    obj['file_extension']
+                )
+        return cls.objs
