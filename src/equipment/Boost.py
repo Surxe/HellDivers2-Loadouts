86c27fdbce8b887aa834c115e22385f64a544681
Ethan <119145352+Surxe@users.noreply.github.com>
2025-02-24 16:15:36 -0500

    save and load methods to classes; create and save in create_objects.py, load in asset_grabber.py (#18)

--- src/equipment/Boost.py
+++ src/equipment/Boost.py
@@ -4,15 +4,19 @@ import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
+# Import external libraries
+import json
+
 # Import local libraries
 from equipment.Equipment import Equipment
 
 class Boost(Equipment):
-    def __init__(self, id, name, wiki_file_name=None):
+    path = "src/equipment/data/boosts.json"
+    objs = {}
+
+    def __init__(self, id, name, wiki_file_name=None, file_extension="svg"):
         if wiki_file_name is None:
             wiki_file_name = name
-
-        file_extension = "svg"
 
         super().__init__(
             id, 
@@ -21,4 +25,19 @@ class Boost(Equipment):
             file_extension=file_extension,
             parent_dirs="boosts".lower().replace(" ", "-")
             )
+        
+        Boost.objs[id] = self
+
+    @classmethod
+    def load(cls):
+        with open(cls.path, 'r') as f:
+            objs = json.load(f)
+            for id, obj in objs.items():
+                cls(
+                    id,
+                    obj['name'],
+                    obj['wiki_file_name'],
+                    obj['file_extension']
+                )
+        return cls.objs
 
--- src/equipment/Stratagem.py
+++ src/equipment/Stratagem.py
@@ -4,10 +4,15 @@ import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
+# Import external libraries
+import json
+
 # Import local libraries
 from equipment.Equipment import Equipment
 
 class Stratagem(Equipment):
+    path = "src/equipment/data/stratagems.json"
+    objs = {}
     valid_departments = ["Patriotic Administration Center", "Orbital Cannons", "Hangar", "Bridge", "Engineering Bay", "Robotics Workshop", "Warbonds"]
 
     def __init__(self, id, name, department, wiki_file_name=None, file_extension="png"):
@@ -26,3 +31,18 @@ class Stratagem(Equipment):
             raise ValueError(f"For stratagem \"{self.id}\", Invalid department {department}, must be one of {self.valid_departments}")
         self.department = department
 
+        Stratagem.objs[id] = self
+
+    @classmethod
+    def load(cls):
+        with open(cls.path, 'r') as f:
+            objs = json.load(f)
+            for id, obj in objs.items():
+                cls(
+                    id,
+                    obj['name'],
+                    obj['department'],
+                    obj['wiki_file_name'],
+                    obj['file_extension']
+                )
+        return cls.objs
