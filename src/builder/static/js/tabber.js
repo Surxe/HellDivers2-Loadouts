bd182f1b1e1675600b45b553377ec6ab6adb890e
Surxe <119145352+Surxe@users.noreply.github.com>
2025-02-26 22:21:35 -0500

    Fix order such that de-selecting tab correctly removes blue

--- src/builder/static/js/tabber.js
+++ src/builder/static/js/tabber.js
@@ -1,22 +1,23 @@
 function tabber_tab_clicked(event, tab_id) {
-    // Check if the tab is active
-    if (document.getElementById(tab_id).classList.contains("active")) {
-        // Hide the tab and return
-        document.getElementById(tab_id).classList.remove("active");
-        return;
-    }
-    
-    // Hide all tab contents
-    document.querySelectorAll(".tab-content").forEach(tab => {
-        tab.classList.remove("active");
-    });
-
     // Remove "active" class from all buttons
     document.querySelectorAll(".tab-button").forEach(button => {
         button.classList.remove("active");
     });
 
+    // Check if the tab is active
+    tab = document.getElementById(tab_id);
+    if (tab.classList.contains("active")) {
+        // Hide the tab and button and return
+        tab.classList.remove("active");
+        return;
+    }
+
+    // Hide all tab contents
+    document.querySelectorAll(".tab-content").forEach(tab => {
+        tab.classList.remove("active");
+    });
+
     // Show the selected tab and mark the button as active
-    document.getElementById(tab_id).classList.add("active");
+    tab.classList.add("active");
     event.currentTarget.classList.add("active");
 }
