818108dcd2552a77ca3d571a2af790a9ef83ee38
Ethan <119145352+Surxe@users.noreply.github.com>
2025-03-24 11:30:17 -0400

    wip show added equipment

--- src/builder/static/js/add_equipment.js
+++ src/builder/static/js/add_equipment.js
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log(selected_equipment);
             let equipment_id = this.id; // Assuming ID is stored in data-tooltip
 
-            if (equipment_id) {
-                selected_equipment.add(equipment_id);
+            if (equipment_id && !selected_equipment.has(equipment_id)) {
+                // Get current slot to add it to
+                let target_slot = document.querySelector(`.selected-equipment .equipment-slot[id="${selected_equipment.size}"]`);
 
-                // Find the corresponding .equipment-slot inside .selected-equipment
-                let target_slot = document.querySelector(`.selected-equipment .equipment-slot[id="${equipment_id}"]`);
+                selected_equipment.add(equipment_id);
                 
                 if (target_slot) {
                     target_slot.style.border = "2px solid red"; // Change CSS to indicate selection
